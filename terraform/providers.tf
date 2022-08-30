cb177711d61c1be4ff86d7c975db4eded3b49379
Amir Djerassi <amir.djerassi@outlook.com>
2022-08-30 12:06:14 +0000

    files update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,5 +11,4 @@ provider "aws" {
   region                   = "eu-central-1"
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "vscode"
 }
